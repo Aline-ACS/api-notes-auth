3bd34043ae0dfa53a19269b6fb10ffb0f4a835a8
Aline-Acs <aline_capoani@hotmail.com>
2020-10-20 11:21:05 -0300

    no message

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -28,7 +28,7 @@ class UserController {
 
       return res.status(201).json({ user });
     } catch (error) {
-      return res.status(417).json({ message: 'Erro ao cadastrar usuário' });
+      return res.status(401).json({ message: 'Erro ao cadastrar usuário' });
     }
   }
 
